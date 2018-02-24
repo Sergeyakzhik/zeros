5d1afe3b4104f5f8b48cd84bc897e20942f646cc
Sergeyakzhik <sergeyakzhik@gmail.com>
2018-02-25 02:01:19 +0300

    Com 7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+    number -= number % 5;
+
+    var n = 0;
+
+    var i = 1;
+
+    while(Math.pow(5, i) < number){
+        n += parseInt(number / Math.pow(5, i));
+        i++;
+    }
+
+    return n;
 }
